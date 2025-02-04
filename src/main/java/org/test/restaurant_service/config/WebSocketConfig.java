9184e74d801c3a252f08ec141b78aae49f8e1ad8
atmosfera <igorangelcev69@gmail.com>
2025-02-05 01:12:07 +0300

    websocket allow address configurer

--- src/main/java/org/test/restaurant_service/config/WebSocketConfig.java
+++ src/main/java/org/test/restaurant_service/config/WebSocketConfig.java
@@ -18,7 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-orders").setAllowedOrigins("http://localhost:63343").withSockJS(); // Точка подключения SockJS
+        registry.addEndpoint("/ws-orders")
+                .setAllowedOrigins("http://localhost:63343", "http://localhost:5500", "http://127.0.0.1:5500")
+                .withSockJS(); // Точка подключения SockJS
     }
 
 }
