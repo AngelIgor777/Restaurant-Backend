c6fd9561de12cb63892d111e3942caf8da4eb1d9
atmosfera <igorangelcev69@gmail.com>
2025-02-11 03:17:57 +0300

    access to web server WebSocketConfig

--- src/main/java/org/test/restaurant_service/config/WebSocketConfig.java
+++ src/main/java/org/test/restaurant_service/config/WebSocketConfig.java
@@ -19,7 +19,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-orders")
-                .setAllowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .setAllowedOrigins("http://195.133.27.38")
+                .setAllowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://195.133.27.38:*")
                 .withSockJS(); // Enable SockJS support
     }
 
