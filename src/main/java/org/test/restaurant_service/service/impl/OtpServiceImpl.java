4bdb6bd493366c7ca34bb3857560a6a6efeb3c1c
atmosfera <igorangelcev69@gmail.com>
2025-04-15 21:10:19 +0300

    feat-(table orders showing and open score for table orders)

--- src/main/java/org/test/restaurant_service/service/impl/OtpServiceImpl.java
+++ src/main/java/org/test/restaurant_service/service/impl/OtpServiceImpl.java
@@ -3,14 +3,10 @@ package org.test.restaurant_service.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.test.restaurant_service.entity.OtpCode;
-import org.test.restaurant_service.entity.User;
 import org.test.restaurant_service.repository.OtpRepository;
 import org.test.restaurant_service.service.OrderService;
 import org.test.restaurant_service.service.OtpService;
-import org.test.restaurant_service.telegram.handling.TelegramBot;
-import org.test.restaurant_service.telegram.util.TextUtil;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
 
