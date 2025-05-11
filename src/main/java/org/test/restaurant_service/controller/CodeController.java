8baaf016d24e75a40d43686f5ba72dd732d0ef79
atmosfera <igorangelcev69@gmail.com>
2025-05-11 12:51:37 +0300

    ws updated

---
+++ src/main/java/org/test/restaurant_service/controller/CodeController.java
@@ -0,0 +1,26 @@
+package org.test.restaurant_service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.test.restaurant_service.service.impl.cache.CodeCacheService;
+
+@RestController
+@RequestMapping("/api/v1/code")
+public class CodeController {
+
+    private final CodeCacheService codeCacheService;
+
+    public CodeController(CodeCacheService codeCacheService) {
+        this.codeCacheService = codeCacheService;
+    }
+
+    @GetMapping
+    @PreAuthorize("@securityService.userIsAdminOrModerator(authentication)")
+    public ResponseEntity<Integer> getValidCode() {
+        int validCode = codeCacheService.getOrderCode().getTrueCode();
+        return ResponseEntity.ok(validCode);
+    }
+}
